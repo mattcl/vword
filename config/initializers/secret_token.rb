67a72bc85f8e240229d4d74431715ad3a032e9c2
Matt Chun-Lum <mchunlum@gmail.com>
2013-01-05 17:30:56 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vword::Application.config.secret_token = '07c1cac5b98dedfa23a12d153383014ef6bef0d2ed86fa2acc1a2b66fea526ba0f5547c1487ddc34ebb2101133163dc8de81876525d4cd54bc9b5aeddffc4f60'
